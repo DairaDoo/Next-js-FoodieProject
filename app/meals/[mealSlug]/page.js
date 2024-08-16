dcef5a54e0c599a1fbb0a8236cf501bb70ac23f9
DairanDoo <your_email@example.com>
2024-08-16 19:10:34 -0400

    MainHeaderLayout separado del layout.js y añadido en un componente. y uso del css usando el classes importando el css como objeto, module.css. Forma más eficiente de importar estilos.

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -5,14 +5,5 @@
 import Link from "next/link";
 
 export default function MealDeatils({props}) {
-    return (
-        <main>
-            <h1 style={{ color: 'white', textAlign: 'center' }}>Meals Dinamic Route.</h1>
-            <p><Link href="/">Home Page</Link></p>
-            <p><Link href="/meals">Meals Page</Link></p>
-            <p><Link href="/meals/share">Share Page</Link></p>
-            <p><Link href="/meals/dinamic">Meals Dinamic Page</Link></p>
-            <p><Link href="/community">Community Page</Link></p>
-        </main>
-    );
+    return <h1>Meal Details</h1>
 }
