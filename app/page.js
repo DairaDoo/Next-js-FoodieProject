071ab2fa019164af319f7ab332fc0dd1f807eb0f
DairanDoo <your_email@example.com>
2024-08-19 15:39:44 -0400

    Nuevos compoonentes de image-slide añadido y estilos precreados a la sección del image.js y page.js, uso del use client para renderizar componentes en el cliente, como los onlclick y useeffect, en resumen: se añadio un slider y gran mejora de estilos.

--- app/page.js
+++ app/page.js
@@ -1,16 +1,55 @@
-import Link from "next/link";
+"use client";// los componentes de react se ejecutan en el servidor (backend). Ahí que especificarle que use un client component.
 
+import Link from "next/link";
+import classes from './page.module.css';
+import ImageSlideshow from "@/components/images/image-slideshow";
+
+// home page: localhost:3000/
 export default function Home() {
   return (
+    <>
+    <header className={classes.header}>
+      <div className={classes.slideshow}>
+        <ImageSlideshow />
+      </div>
+      <div>
+        <div className={classes.hero}>
+          <h1>NextLevel Food for NextLevel Foodies</h1>
+          <p>Test & share food from all over the world.</p>
+        </div>
+        <div className={classes.cta}>
+          <Link href="/community">Join the Community</Link>
+          <Link href="/meals">Explore Meals</Link>
+        </div>
+      </div>
+    </header>
     <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
-        Time to get started!
-      </h1>
-      <p><Link href="/">Home Page</Link></p>
-      <p><Link href="/meals">Meals Page</Link></p>
-      <p><Link href="/meals/share">Share Page</Link></p>
-      <p><Link href="/meals/dinamic">Meals Dinamic Page</Link></p>
-      <p><Link href="/community">Community Page</Link></p>
+      <section className={classes.section}>
+            <h2>How it works</h2>
+            <p>
+              NextLevel Food is a platform for foodies to share their favorite
+              recipes with the world. It&apos;s a place to discover new dishes, and to
+              connect with other food lovers.
+            </p>
+            <p>
+              NextLevel Food is a place to discover new dishes, and to connect
+              with other food lovers.
+            </p>
+          </section>
+
+        <section className={classes.section}>
+          <h2>Why NextLevel Food?</h2>
+          <p>
+            NextLevel Food is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes, and to
+            connect with other food lovers.
+          </p>
+          <p>
+            NextLevel Food is a place to discover new dishes, and to connect
+            with other food lovers.
+          </p>
+        </section>
     </main>
+    </>
   );
 }
