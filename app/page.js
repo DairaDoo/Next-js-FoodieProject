32f30eef2b6224a57bb06da7505fa62fca4af4ef
DairanDoo <your_email@example.com>
2024-08-24 20:15:37 -0400

    Uso de use client en componentes para no explotr su uso y usar mejor el server side para renderizarlos, como un componente intermediario que si recibe el use client para que el padre no. Uso de pathName para obtener rutas y junto con este se creo la animación de hover para indicar la sección en donde el usuario está ubicado.

--- app/page.js
+++ app/page.js
@@ -1,55 +1,56 @@
-"use client";// los componentes de react se ejecutan en el servidor (backend). Ahí que especificarle que use un client component.
+"use client"; // debe ser use client por que tiene un useEffect (si tuviera un onClick también)
+// esto se renderiza en el servidor pero es necesario tenerlo en este caso luego en el client server.
 
 import Link from "next/link";
-import classes from './page.module.css';
+import classes from "./page.module.css";
 import ImageSlideshow from "@/components/images/image-slideshow";
 
 // home page: localhost:3000/
 export default function Home() {
   return (
     <>
-    <header className={classes.header}>
-      <div className={classes.slideshow}>
-        <ImageSlideshow />
-      </div>
-      <div>
-        <div className={classes.hero}>
-          <h1>NextLevel Food for NextLevel Foodies</h1>
-          <p>Test & share food from all over the world.</p>
+      <header className={classes.header}>
+        <div className={classes.slideshow}>
+          <ImageSlideshow />
         </div>
-        <div className={classes.cta}>
-          <Link href="/community">Join the Community</Link>
-          <Link href="/meals">Explore Meals</Link>
+        <div>
+          <div className={classes.hero}>
+            <h1>NextLevel Food for NextLevel Foodies</h1>
+            <p>Test & share food from all over the world.</p>
+          </div>
+          <div className={classes.cta}>
+            <Link href="/community">Join the Community</Link>
+            <Link href="/meals">Explore Meals</Link>
+          </div>
         </div>
-      </div>
-    </header>
-    <main>
-      <section className={classes.section}>
-            <h2>How it works</h2>
-            <p>
-              NextLevel Food is a platform for foodies to share their favorite
-              recipes with the world. It&apos;s a place to discover new dishes, and to
-              connect with other food lovers.
-            </p>
-            <p>
-              NextLevel Food is a place to discover new dishes, and to connect
-              with other food lovers.
-            </p>
-          </section>
-
+      </header>
+      <main>
         <section className={classes.section}>
-          <h2>Why NextLevel Food?</h2>
+          <h2>How it works</h2>
           <p>
             NextLevel Food is a platform for foodies to share their favorite
-            recipes with the world. It&apos;s a place to discover new dishes, and to
-            connect with other food lovers.
+            recipes with the world. It&apos;s a place to discover new dishes,
+            and to connect with other food lovers.
           </p>
           <p>
             NextLevel Food is a place to discover new dishes, and to connect
             with other food lovers.
           </p>
         </section>
-    </main>
+
+        <section className={classes.section}>
+          <h2>Why NextLevel Food?</h2>
+          <p>
+            NextLevel Food is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes,
+            and to connect with other food lovers.
+          </p>
+          <p>
+            NextLevel Food is a place to discover new dishes, and to connect
+            with other food lovers.
+          </p>
+        </section>
+      </main>
     </>
   );
 }
--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,31 +1,34 @@
 import Link from "next/link";
-import classes from './main-header.module.css'; // importamos css como un objeto y añádimos en los classNames.
+import classes from "./main-header.module.css"; // importamos css como un objeto y añádimos en los classNames.
 import Image from "next/image"; // componente image perteneciente a NextJs.
 
 // así se pueden importar imagenes en NextJs (@ es un alias para el root del proyecto).
 import logoImg from "@/assets/logo.png";
 import MainHeaderBackground from "./main-header-background";
-export default function MainHeader() {
-    return (
-        <>
-            <MainHeaderBackground />
-            <header className={classes.header}>
-                <Link className={classes.logo} href="/">
-                    <Image src={logoImg} alt="A plate with food on it" priority />
-                    NextLevel Food
-                </Link>
+import NavLink from "./nav-link"; // usamos esto para no tener que renderizar esta página en el client side.
 
-                <nav className={classes.nav}>
-                    <ul>
-                        <li>
-                            <Link href="/meals">Browse Meals</Link>
-                        </li>
-                        <li>
-                            <Link href="/community">Foodies Community</Link>
-                        </li>
-                    </ul>
-                </nav>
-            </header>
-        </>
-    )
+export default function MainHeader() {
+
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image src={logoImg} alt="A plate with food on it" priority />
+          NextLevel Food
+        </Link>
+
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+                <NavLink href={'/meals'}>Browse Meals</NavLink>
+            </li>
+            <li>
+                <NavLink href={'/community'}>Foodies Community</NavLink>   
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
 }
