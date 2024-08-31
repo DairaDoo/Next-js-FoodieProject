86e725f6ad84da2b3ad3e9e130140eef01ae7a23
DairanDoo <your_email@example.com>
2024-08-31 15:41:10 -0400

    He creado una página de detalles de la comida en Next.js utilizando parámetros especiales que contienen archivos específicos, como por ejemplo, obtener el valor dinámico de la URL [mealSlug] y extraerlo en forma de par clave-valor. Implementé una función de obtención (fetch) en la biblioteca que devuelve los datos de la comida correspondiente a ese slug. Estos datos se reciben en la página y se renderizan en forma de objeto.

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -3,7 +3,39 @@
 // distinto a shares que es una ruta creada.
 
 import Link from "next/link";
+import classes from "./page.module.css";
+import Image from "next/image";
+import { getMeal } from "@/lib/meals";
 
-export default function MealDeatils({props}) {
-    return <h1>Meal Details</h1>
+export default function MealDeatilsPage({ params }) {
+  // NexJs para unas propiedades especiales a archivos especiales (page.js, error.js, etc..)
+  // en este caso parametros en key-value, en este caso mealSlug es el value.
+  const meal = getMeal(params.mealSlug);
+
+  const formattedInstructions = meal.instructions.replace(/\n/g, "<br />");
+
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: formattedInstructions,
+          }}
+        ></p>
+      </main>
+    </>
+  );
 }
