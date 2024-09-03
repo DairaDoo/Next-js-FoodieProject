8d806ea73142a6c866f60f978a8f38956fd0b10a
DairanDoo <your_email@example.com>
2024-09-02 23:16:33 -0400

    Funcionalidad de subir imagenes completada, uso del redirect para enviar imagenes, y ajuste de rutas para no meter la imagenes a la base de datos. Además se uso dos librerias, xss y slugify para evitar cross-script attacks.

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,23 +1,8 @@
 import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
+import { shareMeal } from "@/lib/actions"; // importamos el share meal que enviará la data del form.
 
 export default function ShareMealPage() {
-  async function shareMeal(formData) {
-    "use server"; // server action (garantiza que la función SOLO se ejecutará en el server).
-
-    const meal = {
-      title: formData.get("title"),
-      summary: formData.get("summary"),
-      instructions: formData.get("instructions"),
-      image: formData.get("image"),
-      creator: formData.get("name"),
-      creator_email: formData.get("email"),
-    };
-
-    console.log(meal);
-
-    
-  }
 
   return (
     <>
