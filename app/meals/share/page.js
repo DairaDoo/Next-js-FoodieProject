50dea3cd17f955680e102b20e0b372c6d9962a81
DairanDoo <your_email@example.com>
2024-09-04 12:45:40 -0400

    Error con action state sin solucionar.

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -2,8 +2,12 @@ import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
 import { shareMeal } from "@/lib/actions"; // importamos el share meal que enviará la data del form.
 import MealsFormSubmit from "@/components/meals/meals-form-submit";
+import { useActionState } from 'react';
 
 export default function ShareMealPage() {
+
+  const [state, formAction] = useActionState(shareMeal, {message: null});
+
   return (
     <>
       <header className={classes.header}>
@@ -13,9 +17,9 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
-            <p>
+            <p> 
               <label htmlFor="name">Your name</label>
               <input type="text" id="name" name="name" required />
             </p>
@@ -42,6 +46,7 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker label="Your image" name="image" />
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
             <MealsFormSubmit />
           </p>
